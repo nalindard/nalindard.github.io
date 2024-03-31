0c960a38aa09cb360e67978fc1da55f6e9a7be87
Falcon <nalindard@gmail.com>
2024-03-31 22:12:14 +0530

    Layout updates

--- assets/SurfingView-8rB6xNT9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index-6wKqiaUX.js";const r={};function o(t,s){return n(),c("h2",null,"Surfing")}const a=e(r,[["render",o]]);export{a as default};
--- assets/SurfingView-HDPs6-w0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index--QBftwZ2.js";const r={};function o(t,s){return n(),c("h2",null,"Surfing")}const a=e(r,[["render",o]]);export{a as default};
