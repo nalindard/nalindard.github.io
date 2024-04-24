7a66ce04c4a7fa39bc3c9d595fd463c443e4ac6a
Falcon <nalindard@gmail.com>
2024-04-24 09:56:18 +0530

    responsive

---
+++ _nuxt/BdJyDOYM.js
@@ -0,0 +1,1 @@
+import{_ as o,o as r,a as t,O as a}from"./CYgdlqTX.js";const s={};function n(e,c){return r(),t("thead",null,[a(e.$slots,"default")])}const _=o(s,[["render",n]]);export{_ as default};
---
+++ _nuxt/xd4QfBT7.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,a as c}from"./CYgdlqTX.js";const o={};function t(n,s){return r(),c("hr")}const _=e(o,[["render",t]]);export{_ as default};
